1ca6a158e39a509443c57394abf8eb7b5f58a084
K PIYUSHA <47896745+KPIYUSHA7@users.noreply.github.com>
2020-07-23 13:02:22 +0530

    Update generate_otp.py

--- generate_otp.py
+++ generate_otp.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[31]:
-
-
 import random
-import smtplib
-
-
-# In[26]:
-
-
 otp = ''
 for i in range(6):
     otp = otp + str(random.randint(0,9))
-
-
-# In[30]:
-
-
 otp
 
